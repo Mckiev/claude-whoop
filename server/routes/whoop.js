38e9fb9a47105a7dbb7defe97120bdc88775d521
Anton Makiievskyi <iammckiev@gmail.com>
2024-12-29 08:38:24 -0800

    rolled back explicit variables for db connection

--- server/routes/whoop.js
+++ server/routes/whoop.js
@@ -5,11 +5,15 @@ const { Pool } = require('pg');
 const router = express.Router();
 
 const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: {
       rejectUnauthorized: false
   }
 });
-
 const initDatabase = async () => {
   try {
       await pool.query(`
